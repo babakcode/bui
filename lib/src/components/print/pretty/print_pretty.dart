48d0554f71a9425fcf14cd992d797ef0604b29e2
Babak <babakcode@gmail.com>
2023-07-05 17:46:34 +0330

    complete printPretty with dart 3 `V2`

--- lib/src/components/print/pretty/print_pretty.dart
+++ lib/src/components/print/pretty/print_pretty.dart
@@ -4,27 +4,40 @@ import 'package:flutter/foundation.dart';
 
 export '../../enum/print/print_pretty_colors.dart';
 
-void printPretty(String message,
-    {String? name, PrintPrettyColors? printPrettyColor, dynamic json,}) {
-  if (kDebugMode) {
-    try {
-      if (json is String) {
-        final obj = jsonDecode(json);
-        if (obj is Map || obj is List) {
-          var encoder = const JsonEncoder.withIndent("  ");
-          List linesMessages = encoder.convert(obj).split('\n');
-
-          print("${name == null ? "" : "[$name]"} $message");
-          for (final element in linesMessages) {
-            print(
-                "${printPrettyColor?.color ?? ""}"
-                    "$element${printPrettyColor?.resetColor ?? ""}");
-          }
-        }
-        return;
+void printPretty(
+  String message, {
+  String? name,
+  PrintPrettyColors? printPrettyColor,
+  dynamic json,
+}) {
+  try {
+    if (json is Map || json is List) {
+      return _print(json, name, message, printPrettyColor);
+    } else if (json is String) {
+      final obj = jsonDecode(json);
+      if (obj is Map || obj is List) {
+        return _print(obj, name, message, printPrettyColor);
       }
-    } catch (e) {
-      return;
+    }
+  } catch (e) {
+    return;
+  }
+}
+
+void _print(
+  dynamic obj,
+  String? name,
+  String? message,
+  PrintPrettyColors? printPrettyColor,
+) {
+  if (kDebugMode) {
+    var encoder = const JsonEncoder.withIndent("  ");
+    List linesMessages = encoder.convert(obj).split('\n');
+
+    print("${name == null ? "" : "[$name]"} $message");
+    for (final element in linesMessages) {
+      print("${printPrettyColor?.color ?? ""}"
+          "$element${printPrettyColor?.resetColor ?? ""}");
     }
   }
 }
